fa30447091080704f3a7e5af44e8658df498b0c6
Clinton VanSciver <clintvans@gmail.com>
2017-03-28 09:21:43 -0700

    Updated flickr API key.

--- FlickrCal/Constant.swift
+++ FlickrCal/Constant.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Constant {
-	static let apiKey = "cff116bfa7128f24b37ac7e4c1c62a8b"
+	static let apiKey = "6e8d70d789e1fad1c4063c8622e135fd"
 	static let quoteUrl = "http://quotes.rest/qod.json"
 	static let searchTerm = "cityscape"
 }
